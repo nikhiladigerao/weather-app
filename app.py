ad3f9f457df4eb44658e7516cff336d7ea43c713
Nikhil Rao <nikhilrao@Nikhils-Air.attlocal.net>
2023-11-14 12:01:43 -0800

    created index.html file and added more variables for temperature

--- app.py
+++ app.py
@@ -7,40 +7,43 @@ import os
 # create flask app object
 app = Flask(__name__)
 
-@app.route('/city')
-def search_city():
-
-    ''' 
-    search for a specific city
+@app.route("/city")
+def current_temp():
+    """
+    search for a specific city's current temperature
     param:
         api_key: api key for openweathermap.org
         city: city name
-    '''
+    """
 
     # initialize api key
     api_k = config.api_key
 
     # pass city name as an arg
-    city = request.args.get('q')
+    city = request.args.get("q")
 
     # call api and convert response into dict
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_k}'
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_k}"
     response = requests.get(url).json()
 
     # error if unknown city name or invalid api key
-    if response.get('cod') != 200:
-        message = response.get('message', '')
-        return f'Error getting temperature for {city.title()}. Error message = {message}'
-    
-    # get temperature and description
-    current_temperature = response.get('main', {}).get('temp')
-    current_wind_speed = response.get('wind', {}).get('speed')
+    if response.get("cod") != 200:
+        message = response.get("message", "")
+        return (
+            f"Error getting temperature for {city.title()}. Error message = {message}"
+        )
 
-    if current_temperature:
-        current_temperature_celsius = round(current_temperature - 273.15, 2)
-        return f'The current temperature of {city.title()} is {current_temperature_celsius} &#8451;. The wind speed is {current_wind_speed} mph.'
-    else:
-        return f'Error getting temperature for {city.title()}'
+    # get temperature data
+    list_of_data = response.get("main", {})
 
-if __name__ == '__main__':
+    data = {
+        "current_temperature": str(list_of_data["temp"]) + "K",
+        "current_humidity": str(list_of_data["humidity"]) + "%",
+        "min_temp": str(list_of_data["temp_min"]) + "K",
+        "max_temp": str(list_of_data["temp_max"]) + "K",
+    }
+
+    return render_template("index.html", data=data)
+
+if __name__ == "__main__":
     app.run(debug=True)
